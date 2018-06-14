78bc637ab227eabb45b053cf66a8e02e06808491
Dhruv Murarka <dhruvmurarka@yahoo.co.in>
2018-06-14 23:30:41 +0530

    Updated main page UI

--- js/mainPage1.js
+++ js/mainPage1.js
@@ -1,6 +1,19 @@
 console.log('MainPage');
 
+var currentSort = {
+	sortKey: 'none',
+	sortedIncreasing: false
+}
+var optionalItemKeys = [
+	{keyName: 'timestamp', uiName: 'Time Stamp', show: true, numericSort: false},
+	{keyName: 'timeSpent', uiName: 'Time Spent', show: true, numericSort: true},
+	{keyName: 'copy', uiName: 'Copy', show: true, numericSort: true},
+	{keyName: 'clicks', uiName: 'Clicks', show: true, numericSort: true},
+	{keyName: 'scroll', uiName: 'Scroll', show: true, numericSort: true}
+];
+
 var data = {
+	optionalItemKeys: optionalItemKeys,
 	buckets: [
 		{
 			title: 'Bucket 1',
@@ -18,21 +31,54 @@ var data = {
 					{url: 'https://yahoo.com', title: 'Yahoo', timestamp: '6pm today', timeSpent: '5', copy: '4', clicks: '7', scroll: '180'},
 					{url: 'https://yahoo.com', title: 'Yahoo', timestamp: '6pm today', timeSpent: '7', copy: '1', clicks: '2', scroll: '263'}
 			]
+		},
+		{
+			title: 'Bucket 3',
+			topics: ['C#', 'Azure API'],
+			items: [{url: 'https://yahoo.com', title: 'Yahoo', timestamp: '6pm today', timeSpent: '10', copy: '2', clicks: '5', scroll: '100'},
+					{url: 'https://yahoo.com', title: 'Yahoo', timestamp: '6pm today', timeSpent: '5', copy: '4', clicks: '7', scroll: '180'},
+					{url: 'https://yahoo.com', title: 'Yahoo', timestamp: '6pm today', timeSpent: '7', copy: '1', clicks: '2', scroll: '263'}
+			]
+		},
+		{
+			title: 'Bucket 4',
+			topics: ['C#', 'Azure API'],
+			items: [{url: 'https://yahoo.com', title: 'Yahoo', timestamp: '6pm today', timeSpent: '10', copy: '2', clicks: '5', scroll: '100'},
+					{url: 'https://yahoo.com', title: 'Yahoo', timestamp: '6pm today', timeSpent: '5', copy: '4', clicks: '7', scroll: '180'},
+					{url: 'https://yahoo.com', title: 'Yahoo', timestamp: '6pm today', timeSpent: '7', copy: '1', clicks: '2', scroll: '263'}
+			]
 		}
 	]
 }
 
-var vue = new Vue({
+var contentVM = new Vue({
 	el: '#content',
 	data: data,
 	methods: {
-		sortByNumeric: function (key, increasing) {
-			data.items.sort(function (a, b) {
-  				if(increasing)
-  					return a[key] - b[key];
-  				else
-  					return b[key] - a[key];
-			})
+	}
+})
+
+var headerVM = new Vue({
+	el: '#header',
+	data: { 
+		optionalItemKeys: optionalItemKeys,
+		currentSort: currentSort
+	},
+	methods: {
+		sortByNumeric: function (key) {
+			currentSort.sortedIncreasing = !currentSort.sortedIncreasing;
+			var increasing = currentSort.sortedIncreasing;
+			currentSort.sortKey = key;
+			for(var i=0; i<data.buckets.length; i++) {
+				data.buckets[i].items.sort(function (a, b) {
+					
+	  				if(increasing)
+	  					return a[key] - b[key];
+	  				else
+	  					return b[key] - a[key];
+				});
+			}
+			console.log("sorted by " + key + ", increasing: " + increasing)
 		}
 	}
 })
