a251304e6250854e186bfe14f7b677a5cdac4e85
Karan Jit Singh <karan1866@gmail.com>
2018-06-15 00:51:53 +0530

    data update

--- js/lib/eventLogger.js
+++ js/lib/eventLogger.js
@@ -1,58 +1,59 @@
-var EventLogger = new function() {
+var EventLogger = new function () {
 
     var data = {
-        id: null,
         url: null,
         clickCount: 0,
         copyCount: 0,
         startTime: null,
         maxScrollHeight: 0,
-        lastScrollHeight: 0
+        lastScrollHeight: 0,
+        totalTime: 0
     }
-    
+
     var pageActive = false;
-    
+
     function onClickHandler(e) {
-        data.clickCount ++;
+        data.clickCount++;
     }
-    
+
     function activatePage() {
         pageActive = true;
         data.clickCount = 0;
         data.startTime = Date.now();
     }
-    
+
     function scrollPositionUpdate(e) {
-        var pos = window.scrollY + window.innerHeight/2;
+        var pos = window.scrollY + window.innerHeight / 2;
         data.maxScrollHeight = pos > data.maxScrollHeight ? pos : data.maxScrollHeight;
         data.lastScrollHeight = pos;
     }
-    
+
     function pageDeactivateHandler() {
         pageActive = false;
+        totalTime += Date.now() - data.startTime;
         updateStorage();
     }
 
     function copyEventHandler() {
-        data.copyCount ++;
+        data.copyCount++;
     }
-    
+
     function updateStorage() {
-        
+        Database = chrome.extension.getBackgroundPage().Database;
+        Database.updateStorage(data);
     }
-    
-    this.initLogger = function() {
+
+    this.initLogger = function () {
         document.addEventListener("click", onClickHandler);
         document.addEventListener("beforeunload", pageDeactivateHandler);
         document.addEventListener("scroll", scrollPositionUpdate);
         document.addEventListener("blur", pageDeactivateHandler)
         document.addEventListener("focus", activatePage);
         document.addEventListener("copy", copyEventHandler);
-        
+
         activatePage();
-    
     }
-    
+
     data.url = document.location.href;
 
 };
